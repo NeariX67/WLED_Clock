112d878bc4030625ac3dc806db1a3b570ca1d6cd
NeariX67 <dergamer846@gmail.com>
2020-09-18 23:33:40 +0200

    Fixed Clock effect, added ntp am/pm setting support, no define needed, fixed misbehaviours

--- wled00/FX.cpp
+++ wled00/FX.cpp
@@ -3729,7 +3729,6 @@ const boolean segmentArray[11][7] = {{0, 0, 0, 0, 0, 0, 0}, {1, 1, 1, 0, 1, 1, 1
 
 
 //Comment or uncomment this for your setup!
-#define USE_24H_FORMAT
 //#define USE_COLON_SEPARATOR
 
 uint16_t WS2812FX::mode_clock(void) {
@@ -3739,16 +3738,20 @@ uint16_t WS2812FX::mode_clock(void) {
   uint8_t timeMinute = minute(localTime);
 
   uint8_t ledsPerSegment = 9;       //9 LEDs per Segmentfraction (7 Segment with each 9 LEDs = 63 LEDs)
+  uint8_t numberOfSegments = 4;     //2x Hours, 2x Minutes
 
-  #ifdef USE_24H_FORMAT
-    uint8_t numberOfSegments = 4;     //2x Hours, 2x Minutes, because 24 hour format
-    uint8_t timeHour = hour(localTime);
-  #endif
-  #ifndef USE_24H_FORMAT
-    uint8_t numberOfSegments = 3;     //1.5x Hours, 2x Minutes, because 12 hour format
-    uint8_t timeHour = hour(localTime) % 12;
+  uint8_t timeHour = 0;             //Bypass compiler error
+  uint8_t limit = 0;                //Bypass compiler error, sets it according to 12/24h format
+  
+  if(!useAMPM) {
+    limit = 0;
+    timeHour = hour(localTime);
+  }
+  else {
+    limit = 1;
+    timeHour = hour(localTime) % 12;
     if(timeHour == 0) timeHour = 12;
-  #endif
+  }
 
   #ifdef USE_COLON_SEPARATOR
     uint8_t ledsPerDot = 1;         //1 LED per seconds dot, so 2 in total
@@ -3762,31 +3765,37 @@ uint16_t WS2812FX::mode_clock(void) {
   digits[2] = timeMinute / 10;
   digits[3] = timeMinute % 10;
 
+  /*digits[3] = timeHour / 10;
+  digits[2] = timeHour % 10;
+  digits[1] = timeMinute / 10;
+  digits[0] = timeMinute % 10;*/
+  //Reversed order for my own setup
+
   uint8_t index = 0;
   uint8_t usedIndex = 0;        //Used for better Color Palette usage
   uint8_t counter = (now * ((SEGMENT.speed >> 2) + 2)) & 0xFFFF;
   
-  for (int i = numberOfSegments; i > 0; i--) {      //Cycle through digits
-    for (int j = 0; j < 7; j++) {                   //Cycle through each part of a digit
-      for (int k = 0; k < ledsPerSegment; k++) {    //Cycle through each led per part of a digit
+  for (int i = numberOfSegments - 1; i >= limit; i--) {      //Cycle through digits
+    for (uint8_t j = 0; j < 7; j++) {                   //Cycle through each part of a digit
+      for (uint8_t k = 0; k < ledsPerSegment; k++) {    //Cycle through each led per part of a digit
         if(segmentArray[digits[i]+1][j]) {          //If LEDs should light up:
           usedIndex++;
-          #ifdef USE_24H_FORMAT
+          if(!useAMPM) {
             if(i == 3 || i == 2) {      //Minutes
               setPixelColor(index, color_from_palette(usedIndex*map(SEGMENT.intensity, 0, 255, 0, 25)+(now / map(SEGMENT.speed, 0, 255, 400, 5)), false, true, 0));
             }
             else if(i == 1 || i == 0) { //Hours
               setPixelColor(index, color_from_palette(usedIndex*map(SEGMENT.intensity, 0, 255, 0, 25)+(now / map(SEGMENT.speed, 0, 255, 400, 5)), false, true, 1));
             }
-          #endif
-          #ifndef USE_24H_FORMAT
+          }
+          else {
             if(i == 3 || i == 2) {      //Minutes
               setPixelColor(index, color_from_palette(usedIndex*map(SEGMENT.intensity, 0, 255, 0, 25)+(now / map(SEGMENT.speed, 0, 255, 400, 5)), false, true, 0));
             }
             else if(i == 1) {           //Hours
               setPixelColor(index, color_from_palette(usedIndex*map(SEGMENT.intensity, 0, 255, 0, 25)+(now / map(SEGMENT.speed, 0, 255, 400, 5)), false, true, 1));
             }
-          #endif
+          }
         }
         else {      //LEDs go black
           setPixelColor(index, 0);
@@ -3795,15 +3804,15 @@ uint16_t WS2812FX::mode_clock(void) {
       }
     }
     #ifdef USE_COLON_SEPARATOR
-      if(numberOfSegments - 1 == i) {                                                       //Or this, should result in the same, i will test soon
+      if(numberOfSegments - 2 == i) {
         if(timeSecond % 2 == 0) {                                                           //Light up on even seconds
-          for(int l = 0; l < ledsPerDot*2;l++) {
+          for(uint8_t l = 0; l < ledsPerDot*2;l++) {
             setPixelColor(index, dotColor);
             index++;
           }
         }
         else {
-          for(int l = 0; l < ledsPerDot*2;l++) {
+          for(uint8_t l = 0; l < ledsPerDot*2;l++) {
             setPixelColor(index, 0);
             index++;
           }
@@ -3811,22 +3820,22 @@ uint16_t WS2812FX::mode_clock(void) {
       }
     #endif
   }
-  #ifndef USE_24H_FORMAT
-  if(timeHour >= 10) {
-    for(uint8_t i = 0;i < 2*ledsPerSegment;i++) {
-      setPixelColor(index+i, color_from_palette(usedIndex*map(SEGMENT.intensity, 0, 255, 0, 25)+(now / map(SEGMENT.speed, 0, 255, 400, 5)), false, true, 1));
+  if(useAMPM) {
+    if(digits[0] > 0) {
+      for(uint8_t i = 0;i < 2*ledsPerSegment;i++) {
+        setPixelColor(index+i, color_from_palette(usedIndex*map(SEGMENT.intensity, 0, 255, 0, 25)+(now / map(SEGMENT.speed, 0, 255, 400, 5)), false, true, 1));
+      }
     }
-  }
-  else {
-    for(uint8_t i = 0;i < 2*ledsPerSegment;i++) {
-      setPixelColor(index+i, 0);
+    else {
+      for(uint8_t i = 0;i < 2*ledsPerSegment;i++) {
+        setPixelColor(index+i, 0);
+      }
     }
+    index += 2*ledsPerSegment;
   }
-  index += 2*ledsPerSegment;
-  #endif
 
   
-  for(int i = index; i < SEGLEN;i++) {
+  for(uint16_t i = index; i < SEGLEN;i++) {
     setPixelColor(i, SEGCOLOR(2));
   }
 
